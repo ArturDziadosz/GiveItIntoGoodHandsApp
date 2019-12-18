6547f5fe7b77de288f116fd60aab61b304dd8e52
ArturDziadosz <artur.dziadosz1@gmail.com>
2019-12-18 13:58:40 +0100

    GIIGH App v.0.2.14 Added:  - simple validation of inputs before fetching data to Login.js and Register.js.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -6,7 +6,59 @@ import {CtaSignIn} from "../CTA/CtaSignIn";
 import './Login.scss';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      emailError: "",
+      password: "",
+      passwordError: ""
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const {email, password} = this.state;
+    let error = false;
+
+    this.setState({
+      emailError: "",
+      passwordError: ""
+    });
+
+    if (!this.validateEmail(email)) {
+      this.setState({
+        emailError: "Podany email jest nieprawidłowy!"
+      });
+      error = true;
+    }
+    if (password.length < 6) {
+      this.setState({
+        passwordError: "Podane hasło jest za krótkie!"
+      });
+      error = true;
+    }
+
+    if (error) {
+      return false;
+    }
+    //  FETCH
+    console.log("OK");
+  };
+
   render() {
+    const {email, emailError, password, passwordError} = this.state;
     return (
       <>
         <section className="container containerAlt">
@@ -19,20 +71,24 @@ class Login extends Component {
           <div className="logInBox">
             <h1 className="title">Zaloguj się</h1>
             <div className="decoration"/>
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
               <div className="formBox">
                 <label>
                   Email<br/>
-                  <input/>
+                  <input type={"text"} name={"email"} value={email} onChange={this.handleChange}
+                         style={emailError ? {borderBottom: "1px solid red"} : null}/>
+                  {emailError ? <p>{emailError}</p> : null}
                 </label>
                 <label>
                   Hasło<br/>
-                  <input/>
+                  <input type={"password"} name={"password"} value={password} onChange={this.handleChange}
+                         style={passwordError ? {borderBottom: "1px solid red"} : null}/>
+                  {passwordError ? <p>{passwordError}</p> : null}
                 </label>
               </div>
               <div className="formSubmit">
                 <CtaSignIn/>
-                <button className={"logInBtn"}><span>Zaloguj się</span></button>
+                <button className={"logInBtn"}>Zaloguj się</button>
               </div>
             </form>
           </div>
--- src/components/login/Register.js
+++ src/components/login/Register.js
@@ -6,7 +6,69 @@ import {CtaLogIn} from "../CTA/CtaLogIn";
 import './Login.scss';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      emailError: "",
+      password: "",
+      passwordError: "",
+      password2: "",
+      passwordError2: ""
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const {email, password, password2} = this.state;
+    let error = false;
+
+    this.setState({
+      emailError: "",
+      passwordError: "",
+      passwordError2: ""
+    });
+
+    if (!this.validateEmail(email)) {
+      this.setState({
+        emailError: "Podany email jest nieprawidłowy!"
+      });
+      error = true;
+    }
+    if (password.length < 6) {
+      this.setState({
+        passwordError: "Podane hasło jest za krótkie!"
+      });
+      error = true;
+    }
+
+    if (password2.length < 6 || password !== password2) {
+      this.setState({
+        passwordError2: "Podane hasło jest nieprawidłowe!"
+      });
+      error = true;
+    }
+
+    if (error) {
+      return false;
+    }
+    //  FETCH
+    console.log("OK");
+  };
+
   render() {
+    const {email, emailError, password, passwordError, password2, passwordError2} = this.state;
     return (
       <>
         <section className="container containerAlt">
@@ -19,24 +81,29 @@ class Register extends Component {
           <div className="logInBox">
             <h1 className="title">Załóż konto</h1>
             <div className="decoration"/>
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
               <div className="formBox">
                 <label>
                   Email<br/>
-                  <input/>
+                  <input type={"text"} name={"email"} value={email} onChange={this.handleChange} style={emailError ? {borderBottom: "1px solid red"} : null}/>
+                  {emailError ? <p>{emailError}</p> : null}
                 </label>
                 <label>
                   Hasło<br/>
-                  <input/>
+                  <input type={"password"} name={"password"} value={password} onChange={this.handleChange}
+                         style={passwordError ? {borderBottom: "1px solid red"} : null}/>
+                  {passwordError ? <p>{passwordError}</p> : null}
                 </label>
                 <label>
                   Powtórz hasło<br/>
-                  <input/>
+                  <input type={"password"} name={"password2"} value={password2} onChange={this.handleChange}
+                         style={passwordError2 ? {borderBottom: "1px solid red"} : null}/>
+                  {passwordError2 ? <p>{passwordError2}</p> : null}
                 </label>
               </div>
               <div className="formSubmit">
                 <CtaLogIn text={"Zaloguj się"}/>
-                <div className={"logInBtn"}>Załóż konto</div>
+                <button className={"logInBtn"}>Załóż konto</button>
               </div>
             </form>
           </div>
