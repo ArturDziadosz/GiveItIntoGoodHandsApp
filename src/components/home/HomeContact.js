9852b0a5ae524d16b8fc127bd44a1cf0babc9d65
ArturDziadosz <artur.dziadosz1@gmail.com>
2019-12-18 14:23:06 +0100

    GIIGH App v.0.2.15 Added:  - simple validation of inputs before fetching data to HomeContact.js.

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -3,29 +3,95 @@ import React, {Component} from "react";
 import './HomeContact.scss';
 
 class HomeContact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      nameError: "",
+      email: "",
+      emailError: "",
+      textarea: "",
+      textareaError: ""
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const {name, email, textarea} = this.state;
+    let error = false;
+
+    this.setState({
+      nameError: "",
+      emailError: "",
+      textareaError: ""
+    });
+    if (name.length === 0) {
+      this.setState({
+        nameError: "Podane imię jest nieprawidłowe!"
+      });
+      error = true;
+    }
+    if (!this.validateEmail(email)) {
+      this.setState({
+        emailError: "Podany email jest nieprawidłowy!"
+      });
+      error = true;
+    }
+    if (textarea.length < 120) {
+      this.setState({
+        textareaError: "Wiadomość musi mieć conajmniej 120 znaków!"
+      });
+      error = true;
+    }
+
+    if (error) {
+      return false;
+    }
+    //  FETCH
+    console.log("OK");
+  };
+
   render() {
+    const {name, nameError, email, emailError, textarea, textareaError} = this.state;
     return (
       <section className={"contact"} id={"contact"}>
         <div className="container">
           <div className="row rowContact">
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
               <h2>Skontaktuj się z nami</h2>
               <div className="decoration" />
               <div className="inputs">
                 <div className="input name">
                   <label>Wpisz swoje imię<br/>
-                    <input placeholder={"Artur"} required/>
+                    <input placeholder={"Artur"} type={"text"} name={"name"} value={name} onChange={this.handleChange}
+                           style={nameError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null}/>
+                    {nameError ? <p>{nameError}</p> : null}
                   </label>
                 </div>
                 <div className="input email">
                   <label>Wpisz swój email<br/>
-                    <input placeholder={"abc@xyz.pl"} required/>
+                    <input placeholder={"abc@xyz.pl"} type={"text"} name={"email"} value={email} onChange={this.handleChange}
+                           style={emailError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null}/>
+                    {emailError ? <p>{emailError}</p> : null}
                   </label>
                 </div>
               </div>
               <div className="text">
                 <label>Wpisz swoją wiadomość
-                  <textarea required placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+                  <textarea name={"textarea"} value={textarea} onChange={this.handleChange}
+                            style={textareaError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null} placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+                  {textareaError ? <p>{textareaError}</p> : null}
                 </label>
               </div>
               <button>Wyślij</button>
