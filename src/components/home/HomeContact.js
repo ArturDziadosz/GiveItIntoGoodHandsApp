dc4db03688f32ebf633f63f3244dec5057e8d3f3
ArturDziadosz <artur.dziadosz1@gmail.com>
2019-12-19 17:23:44 +0100

    GIIGH App v.1.0.0 Project done, but without:  - RWD,  - form "Give things",  - login and register panel. Added:  - fetch to contact panel.

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -11,7 +11,9 @@ class HomeContact extends Component {
       email: "",
       emailError: "",
       textarea: "",
-      textareaError: ""
+      textareaError: "",
+      fetchError: "",
+      success: false
     }
   }
 
@@ -34,7 +36,9 @@ class HomeContact extends Component {
     this.setState({
       nameError: "",
       emailError: "",
-      textareaError: ""
+      textareaError: "",
+      fetchError: "",
+      success: false
     });
     if (name.length === 0) {
       this.setState({
@@ -59,18 +63,56 @@ class HomeContact extends Component {
       return false;
     }
     //  FETCH
-    console.log("OK");
+    fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        email,
+        message: textarea
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    }).then(resp => resp.json()).then(data => {
+      if (data.status === "error") {
+        this.setState({
+          fetchError: data.errors.map((msg ,idx) => {
+            return <p key={idx}>{msg.msg}</p>
+          })
+        });
+        return false;
+      }
+
+      if (data.status === "success") {
+        this.setState({
+          success: true,
+          name: "",
+          email: "",
+          textarea: ""
+        });
+        return false;
+      }
+      this.setState({
+        fetchError: "Something goes wrong. Try again later."
+      })
+    }).catch(error => {
+      this.setState({
+        fetchError: error
+      })
+    });
   };
 
   render() {
-    const {name, nameError, email, emailError, textarea, textareaError} = this.state;
+    const {name, nameError, email, emailError, textarea, textareaError, success, fetchError} = this.state;
     return (
       <section className={"contact"} id={"contact"}>
         <div className="container">
           <div className="row rowContact">
             <form onSubmit={this.handleFormSubmit}>
               <h2>Skontaktuj się z nami</h2>
-              <div className="decoration" />
+              <div className="decoration"/>
+              {success ? <div className={"success"}>Wiadomość została wysłana! Wkrótce się skontaktujemy.</div> : null}
+              {fetchError ? <div className={"success"} style={{color: "rgba(255,0,0,.7)"}}>{fetchError}</div> : null}
               <div className="inputs">
                 <div className="input name">
                   <label>Wpisz swoje imię<br/>
@@ -81,7 +123,8 @@ class HomeContact extends Component {
                 </div>
                 <div className="input email">
                   <label>Wpisz swój email<br/>
-                    <input placeholder={"abc@xyz.pl"} type={"text"} name={"email"} value={email} onChange={this.handleChange}
+                    <input placeholder={"abc@xyz.pl"} type={"text"} name={"email"} value={email}
+                           onChange={this.handleChange}
                            style={emailError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null}/>
                     {emailError ? <p>{emailError}</p> : null}
                   </label>
@@ -90,7 +133,8 @@ class HomeContact extends Component {
               <div className="text">
                 <label>Wpisz swoją wiadomość
                   <textarea name={"textarea"} value={textarea} onChange={this.handleChange}
-                            style={textareaError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null} placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+                            style={textareaError ? {borderBottom: "1px solid rgba(255,0,0,.7)"} : null}
+                            placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
                   {textareaError ? <p>{textareaError}</p> : null}
                 </label>
               </div>
